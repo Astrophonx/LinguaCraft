60114873ed3c92ff39342017013b0fc31326efd8
Hristo Petroff <sweet.scope@yahoo.com>
2024-05-30 21:56:09 +0200

    fixed the chat bubbles not to overlap, default max is 3 bubbles, also added auto grow or strink of the chat bubbles depending of the text length

--- src/main/java/dev/astrophonx/linguacraft/eventHandlers/ChatEventHandler.java
+++ src/main/java/dev/astrophonx/linguacraft/eventHandlers/ChatEventHandler.java
@@ -13,6 +13,7 @@ import java.util.*;
 public class ChatEventHandler {
     private static final Logger LOGGER = LogManager.getLogger();
     private static final Map<UUID, List<String>> playerChatBubbles = new HashMap<>();
+    private static final int MAX_CHAT_BUBBLES = 3; // Maximum number of chat bubbles to display
 
     @SubscribeEvent
     public void onServerChat(ServerChatEvent event) {
@@ -21,10 +22,13 @@ public class ChatEventHandler {
         ServerPlayer player = event.getPlayer();
         UUID playerUUID = player.getUUID();
 
-        // Add chat bubble
+        // Add chat bubble and limit the number of chat bubbles to MAX_CHAT_BUBBLES
         playerChatBubbles.computeIfAbsent(playerUUID, k -> new ArrayList<>()).add(message);
+        if (playerChatBubbles.get(playerUUID).size() > MAX_CHAT_BUBBLES) {
+            playerChatBubbles.get(playerUUID).remove(0); // Remove the oldest message if the limit is exceeded
+        }
 
-        // Schedule removal of the chat bubble after 5 seconds
+        // Schedule removal of the chat bubble after 7 seconds by default
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
@@ -38,7 +42,7 @@ public class ChatEventHandler {
                 }
                 LOGGER.info("Removed chat bubble for player: " + playerUUID);
             }
-        }, 5000);
+        }, 7000);
     }
 
     public static Map<UUID, List<String>> getPlayerChatBubbles() {
--- src/main/java/dev/astrophonx/linguacraft/renderes/ChatBubbleRenderer.java
+++ src/main/java/dev/astrophonx/linguacraft/renderes/ChatBubbleRenderer.java
@@ -13,6 +13,7 @@ import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderNameplateEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -22,8 +23,6 @@ import java.util.UUID;
 
 @Mod.EventBusSubscriber(modid = LinguaCraft.MODID, value = Dist.CLIENT)
 public class ChatBubbleRenderer {
-
-    private static final ResourceLocation CHAT_BUBBLE_TEXTURE = new ResourceLocation(LinguaCraft.MODID, "textures/gui/chat_bubble.png");
 
     @SubscribeEvent
     public static void onRenderPlayer(RenderPlayerEvent.Post event) {
@@ -36,11 +35,7 @@ public class ChatBubbleRenderer {
         List<String> chatBubbles = ChatEventHandler.getPlayerChatBubbles().get(playerUUID);
         if (chatBubbles != null && !chatBubbles.isEmpty()) {
             poseStack.pushPose();
-
-            // Calculate the height of the nameplate
-            double nameplateHeight = player.getBbHeight() + 0.25D;
-
-            poseStack.translate(0.0D, nameplateHeight, 0.0D); // Adjust position above nameplate
+            poseStack.translate(0.0D, player.getBbHeight() + 0.75D, 0.0D); // Adjust position above player
             poseStack.mulPose(renderManager.cameraOrientation());
             poseStack.scale(-0.025F, -0.025F, 0.025F);
 
@@ -52,31 +47,39 @@ public class ChatBubbleRenderer {
 
     private static void renderChatBubbles(PoseStack poseStack, MultiBufferSource buffer, List<String> chatBubbles) {
         Font font = Minecraft.getInstance().font;
-        int maxLineWidth = chatBubbles.stream().mapToInt(font::width).max().orElse(0);
         int lineHeight = 10; // Height of each line
+        int bubbleSpacing = 3; // Spacing between bubbles
 
         int backgroundPadding = 4;
-        int backgroundHeight = chatBubbles.size() * lineHeight + backgroundPadding * 2;
-        int backgroundWidth = maxLineWidth + backgroundPadding * 2;
 
-        // Render background
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderTexture(0, CHAT_BUBBLE_TEXTURE);
+        // Limit the chat bubbles to be rendered to the most recent MAX_CHAT_BUBBLES
+        int numBubbles = Math.min(chatBubbles.size(), 3);
+        int yOffset = 0;
+        for (int i = 0; i < numBubbles; i++) {
+            String chatBubble = chatBubbles.get(numBubbles - 1 - i); // Render the most recent message at the top
+            int bubbleHeight = lineHeight + backgroundPadding * 2;
+            int backgroundWidth = font.width(chatBubble) + backgroundPadding * 2;
+            renderBackground(poseStack, backgroundWidth, bubbleHeight, yOffset);
+            font.drawInBatch(chatBubble, -font.width(chatBubble) / 2, yOffset + backgroundPadding + (bubbleHeight - lineHeight) / 2 - 2, 0xFFFFFF, false, poseStack.last().pose(), buffer, false, 0, 15728880);
+            yOffset -= (bubbleHeight + bubbleSpacing);
+        }
+    }
+
+    private static void renderBackground(PoseStack poseStack, int width, int height, int y) {
         Matrix4f matrix = poseStack.last().pose();
         Tesselator tesselator = Tesselator.getInstance();
         BufferBuilder bufferBuilder = tesselator.getBuilder();
-        bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX_COLOR);
-        bufferBuilder.vertex(matrix, -backgroundWidth / 2.0F, -backgroundHeight, 0).uv(0, 1).color(255, 255, 255, 255).endVertex();
-        bufferBuilder.vertex(matrix, -backgroundWidth / 2.0F, 0, 0).uv(0, 0).color(255, 255, 255, 255).endVertex();
-        bufferBuilder.vertex(matrix, backgroundWidth / 2.0F, 0, 0).uv(1, 0).color(255, 255, 255, 255).endVertex();
-        bufferBuilder.vertex(matrix, backgroundWidth / 2.0F, -backgroundHeight, 0).uv(1, 1).color(255, 255, 255, 255).endVertex();
-        tesselator.end();
 
-        // Render each line of chat
-        int y = -backgroundHeight + backgroundPadding;
-        for (String chatBubble : chatBubbles) {
-            font.drawInBatch(chatBubble, -font.width(chatBubble) / 2, y, 0xFFFFFF, false, poseStack.last().pose(), buffer, false, 0, 15728880);
-            y += lineHeight;
-        }
+        RenderSystem.setShader(GameRenderer::getPositionColorShader);
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
+        bufferBuilder.vertex(matrix, -width / 2.0F, y, 0).color(0, 0, 0, 77).endVertex(); // 77 alpha for 0.3 transparency
+        bufferBuilder.vertex(matrix, -width / 2.0F, y + height, 0).color(0, 0, 0, 77).endVertex();
+        bufferBuilder.vertex(matrix, width / 2.0F, y + height, 0).color(0, 0, 0, 77).endVertex();
+        bufferBuilder.vertex(matrix, width / 2.0F, y, 0).color(0, 0, 0, 77).endVertex();
+        tesselator.end();
+        RenderSystem.disableBlend();
     }
+
 }
